b020f078db826ea44c604784f7e157688fe0b07f
Kenzy <tobanghi@gmail.com>
2023-12-24 01:45:57 +0700

    [Add] Kenzy shop

---
+++ Kenzy-shop/src/main/java/com/vti/Kenzy/shop/KenzyShopApplication.java
@@ -0,0 +1,13 @@
+package com.vti.Kenzy.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenzyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenzyShopApplication.class, args);
+	}
+
+}
---
+++ Kenzy-shop/src/test/java/com/vti/Kenzy/shop/KenzyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.Kenzy.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenzyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
