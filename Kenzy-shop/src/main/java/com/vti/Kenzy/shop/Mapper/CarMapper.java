e11d1ff1229b91fd1e22800574d83d026bd90de1
Kenzy <tobanghi@gmail.com>
2023-12-26 20:05:49 +0700

    [Add] Car Mapper

---
+++ Kenzy-shop/src/main/java/com/vti/Kenzy/shop/Mapper/CarMapper.java
@@ -0,0 +1,33 @@
+package com.vti.Kenzy.shop.Mapper;
+
+import com.vti.Kenzy.shop.Dto.CarDto;
+import com.vti.Kenzy.shop.Entity.Car;
+import com.vti.Kenzy.shop.Form.CarCreateForm;
+
+public class CarMapper
+{
+    public static Car map(CarCreateForm form)
+    {
+        var car = new Car();
+        var pk = new Car.PrimaryKey();
+        pk.setLicensePlate(form.getLicensePlate());
+        pk.setRepairDate(form.getRepairDate());
+        car.setPk(pk);
+        car.setCustomerName(form.getCustomerName());
+        car.setCatalogs(form.getCatalogs());
+        car.setCarMaker(form.getCarMaker());
+        return car;
+    }
+
+    public static CarDto map(Car car)
+    {
+        var dto = new CarDto();
+        var pk = car.getPk();
+        dto.setLicensePlate(pk.getLicensePlate());
+        dto.setRepairDate(pk.getRepairDate());
+        dto.setCustomerName(car.getCustomerName());
+        dto.setCatalogs(car.getCatalogs());
+        dto.setCarMaker(car.getCarMaker());
+        return dto;
+    }
+}
