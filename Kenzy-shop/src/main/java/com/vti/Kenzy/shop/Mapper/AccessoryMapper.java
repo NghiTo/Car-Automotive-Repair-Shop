76aac43d62a3daaa5dae766e4b674d61597798bf
Kenzy <tobanghi@gmail.com>
2024-01-04 19:41:04 +0700

    [Add] Accessory Mapper

--- Kenzy-shop/src/main/java/com/vti/Kenzy/shop/Mapper/AccessoryMapper.java
+++ Kenzy-shop/src/main/java/com/vti/Kenzy/shop/Mapper/AccessoryMapper.java
@@ -1,2 +1,39 @@
-package com.vti.Kenzy.shop.Mapper;public class AccessoryMapper {
+package com.vti.Kenzy.shop.Mapper;
+
+import com.vti.Kenzy.shop.Dto.AccessoryDto;
+import com.vti.Kenzy.shop.Entity.Accessory;
+import com.vti.Kenzy.shop.Form.AccessoryCreateForm;
+import com.vti.Kenzy.shop.Form.AccessoryUpdateForm;
+
+public class AccessoryMapper
+{
+    public static Accessory map(AccessoryCreateForm form)
+    {
+        var accessory = new Accessory();
+        accessory.setName(form.getName());
+        accessory.setPrice(form.getPrice());
+        accessory.setStatusDamaged(form.getStatusDamaged());
+        accessory.setRepairStatus(form.getRepairStatus());
+        return accessory;
+    }
+    public static AccessoryDto map(Accessory accessory)
+    {
+        var dto = new AccessoryDto();
+        dto.setId(accessory.getId());
+        var carPk = accessory.getCar().getPk();
+        dto.setLicensePlate(carPk.getLicensePlate());
+        dto.setRepairDate(carPk.getRepairDate());
+        dto.setName(accessory.getName());
+        dto.setPrice(accessory.getPrice());
+        dto.setStatusDamaged(accessory.getStatusDamaged());
+        dto.setRepairStatus(accessory.getRepairStatus());
+        return dto;
+    }
+    public static void map(AccessoryUpdateForm form, Accessory accessory)
+    {
+        accessory.setName(form.getName());
+        accessory.setPrice(form.getPrice());
+        accessory.setStatusDamaged(form.getStatusDamaged());
+        accessory.setRepairStatus(form.getRepairStatus());
+    }
 }
