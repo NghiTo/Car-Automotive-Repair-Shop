615cae2424f0abbeea5b4bccfa951993f9cb7e4e
Kenzy <tobanghi@gmail.com>
2024-01-04 19:37:39 +0700

    [Add] Accessory Service

--- Kenzy-shop/src/main/java/com/vti/Kenzy/shop/Service/AccessoryService.java
+++ Kenzy-shop/src/main/java/com/vti/Kenzy/shop/Service/AccessoryService.java
@@ -1,2 +1,18 @@
-package com.vti.Kenzy.shop.Service;public class AccessoryService {
+package com.vti.Kenzy.shop.Service;
+
+import com.vti.Kenzy.shop.Dto.AccessoryDto;
+import com.vti.Kenzy.shop.Form.AccessoryCreateForm;
+import com.vti.Kenzy.shop.Form.AccessoryUpdateForm;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface AccessoryService
+{
+    Page<AccessoryDto> findAll(Pageable pageable);
+
+    AccessoryDto create(AccessoryCreateForm form);
+
+    AccessoryDto update(AccessoryUpdateForm form, Long id);;
+
+    void deleteById(Long id);
 }
